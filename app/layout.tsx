2e5c060b774b93f7420963345f67d9f6711b0ca8
Sajgroundhogday <sajgroundhogday@gmail.com>
2024-10-27 22:42:27 +0530

    created a theme toggler and removed some styles in globals.css for fixing errors temporarily

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import React from 'react';
+
 import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
@@ -39,7 +40,6 @@ export default function RootLayout({
           <Navbar/>
           {children}
         </ThemeProvider>
-        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 export default function Home() {
-  return (
-    <>
-      <h1 className='h1-bold'>Welcome home</h1>
-      <h1 className='h1-bold font-inter'>Welcome home</h1>
-    </>
-  )
+  return null;
   
 }
